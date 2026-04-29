90e619d36822bebdffa8458f7fb39d0ed6770448
rodenlusica-lab <roden.lusica@csucc.edu.ph>
2026-04-29 01:57:12 -1200

    Update db_config.php

--- inventory-system/php-app/db_config.php
+++ inventory-system/php-app/db_config.php
@@ -1,11 +1,8 @@
 <?php
-// Gikan sa imong InfinityFree 'MySQL Connection Details'
-$servername = "sql211.infinityfree.com"; 
-$username = "if0_41549894";             
-$dbname = "if0_41549894_cbism_db";      
-
-// Ang MySQL Password nimo: I-click ang 'eye' icon sa image_80fe64.png para makita
-$password = "lusicafamily"; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cloud_system";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
